9242b4364a3c499475b7ee5d4990c6efbd88921d
German M. Bravo <german.mb@deipi.com>
2011-05-28 12:32:03 -0500

    Improvements added for sticky elements that move in the page, they're now kept sticked properly

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -1,7 +1,8 @@
 // Sticky Plugin
 // =============
 // Author: Anthony Garand
-// Date: 2/14/2011
+// Improvements by German M. Bravo (Kronuz)
+// Date: 5/28/2011
 // Website: http://labs.anthonygarand.com/sticky
 // Description: Makes an element on the page stick on the screen
 
@@ -15,27 +16,27 @@
 		return this.each(function() {
 			var topSpacing = o.topSpacing,
 			stickyElement = $(this),
-			regPosition = stickyElement.offset().top,
 			stickyId = stickyElement.attr('id'),
 			fixed = false;
-			stickyElement.wrapAll('<div id="' + stickyId + 'StickyWrapper"></div>');
 			stickyElement
-				.css('width', stickyElement.width())
-				.parent()
-					.css('width', stickyElement.outerWidth())
-					.css('height', stickyElement.outerHeight())
-					.css('clear', stickyElement.css('clear'));
+				.wrapAll('<div id="' + stickyId + 'StickyWrapper"></div>')
+				.css('width', stickyElement.width());
+			var stickyWrapper = stickyElement.parent();
+			stickyWrapper
+				.css('width', stickyElement.outerWidth())
+				.css('height', stickyElement.outerHeight())
+				.css('clear', stickyElement.css('clear'));
 			$(window).scroll(function() {
 				var scrollTop = $(window).scrollTop();
-				if (scrollTop <= regPosition - topSpacing) {
+				var elementTop = stickyWrapper.offset().top;
+				if (scrollTop <= elementTop - topSpacing) {
 					if (fixed) {
 						stickyElement.css('position', '').css('top', '').removeClass(o.className);
 						fixed = false;
 					}
 				}
 				else if (!fixed) {
-					var elementPosition = stickyElement.offset().top - scrollTop;
-					if (elementPosition <= topSpacing) {
+					if (scrollTop >= elementTop - topSpacing) {
 						stickyElement.css('position', 'fixed').css('top', topSpacing).addClass(o.className);
 						fixed = true;
 					}
