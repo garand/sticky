91a4e3c91ed1237b7b8c52bc8f74b712faf81e32
German M. Bravo <german.mb@deipi.com>
2011-12-09 13:55:00 -0600

    bottomSpacing fixed and some optimizations added

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -2,7 +2,7 @@
 // =============
 // Author: Anthony Garand
 // Improvements by German M. Bravo (Kronuz)
-// Date: 11/05/2011
+// Date: 9/12/2011
 // Website: http://labs.anthonygarand.com/sticky
 // Description: Makes an element on the page stick on the screen
 
@@ -12,14 +12,14 @@
 			topSpacing: 0,
 			bottomSpacing: 0,
 			className: 'is-sticky'
-		};  
+		};
 		var o = $.extend(defaults, options);
 		return this.each(function() {
 			var topSpacing = o.topSpacing,
 				bottomSpacing = o.bottomSpacing,
 				stickyElement = $(this),
 				stickyId = stickyElement.attr('id'),
-				fixed = null;
+				currentTop = null;
 			stickyElement
 				.wrapAll('<div id="' + stickyId + 'StickyWrapper"></div>')
 				.css('width', stickyElement.width());
@@ -29,28 +29,33 @@
 				.css('width', stickyElement.outerWidth())
 				.css('height', elementHeight)
 				.css('clear', stickyElement.css('clear'));
+			var windowHeight = $(window).height();
+			$(window).resize(function() {
+				windowHeight = $(window).height();
+			});
 			$(window).scroll(function() {
 				var scrollTop = $(window).scrollTop(),
-					windowHeight = $(window).height(),
 					documentHeight = $(document).height(),
-					elementTop = stickyWrapper.offset().top;
-				if (scrollTop <= elementTop - topSpacing) {
-					if (fixed !== null) {
+					elementTop = stickyWrapper.offset().top,
+					dwh = documentHeight - windowHeight;
+				var extra = (scrollTop > dwh) ? dwh - scrollTop : 0;
+				var etse = elementTop - topSpacing - extra;
+				if (scrollTop <= etse) {
+					if (currentTop !== null) {
 						stickyElement.css('position', '').css('top', '').removeClass(o.className);
-						fixed = null;
+						currentTop = null;
 					}
 				}
 				else {
-					if (scrollTop + topSpacing >= elementTop) {
-						var extra = (documentHeight < scrollTop + windowHeight) ? documentHeight - (scrollTop + windowHeight) : 0;
-						var newTop = (documentHeight - elementHeight - bottomSpacing) - scrollTop - extra;
-						if (newTop < 0) {
-							stickyElement.css('position', 'fixed').css('top', newTop + topSpacing).addClass(o.className);
-							fixed = false;
-						} else if (!fixed) {
-							stickyElement.css('position', 'fixed').css('top', topSpacing).addClass(o.className);
-							fixed = true;
-						}
+					var newTop = documentHeight - elementHeight - topSpacing - bottomSpacing - scrollTop - extra;
+					if (newTop < 0) {
+						newTop = newTop + topSpacing;
+					} else {
+						newTop = topSpacing;
+					}
+					if (currentTop != newTop) {
+						stickyElement.css('position', 'fixed').css('top', newTop).addClass(o.className);
+						currentTop = newTop;
 					}
 				}
 			});
