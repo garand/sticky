69b8334d26d648cdbc05a3e4956fab677c1467f8
Matt Sephton <matt.sephton@gmail.com>
2012-08-30 16:07:48 +0100

    remove parent, bump date

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -3,8 +3,8 @@
 // Author: Anthony Garand
 // Improvements by German M. Bravo (Kronuz) and Ruud Kamphuis (ruudk)
 // Improvements by Leonardo C. Daronco (daronco)
-// Created: 2/14/2011
-// Date: 2/12/2012
+// Created: 2011-02-14
+// Date: 2012-08-30
 // Website: http://labs.anthonygarand.com/sticky
 // Description: Makes an element on the page stick on the screen as you scroll
 //              It will only set the 'top' and 'position' of your element, you
@@ -93,6 +93,9 @@
 
                     stickyElement.css('position', '').css('top', '').removeClass(o.elementClassName);
                     stickyWrapper.removeClass(o.wrapperClassName +' '+ o.elementClassName);
+
+                    var wrapperContents = stickyWrapper.contents();
+                    stickyWrapper.replaceWith(wrapperContents);
 
                     if (window.removeEventListener) {
                         window.removeEventListener('scroll', scroller, false);
