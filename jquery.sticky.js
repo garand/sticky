60fedabdd327b553e52ec23383aeea4b110b5a3d
Dumitru Uzun <contact@duzun.me>
2015-04-15 17:27:45 +0300

    .unstick() removes all appearences of elements from sticked array

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -1,16 +1,19 @@
-// Sticky Plugin v1.0.0 for jQuery
+// Sticky Plugin v1.0.1 for jQuery
 // =============
 // Author: Anthony Garand
 // Improvements by German M. Bravo (Kronuz) and Ruud Kamphuis (ruudk)
 // Improvements by Leonardo C. Daronco (daronco)
 // Created: 2/14/2011
-// Date: 2/12/2012
+// Date: 15/04/2015
 // Website: http://labs.anthonygarand.com/sticky
 // Description: Makes an element on the page stick on the screen as you scroll
 //       It will only set the 'top' and 'position' of your element, you
 //       might need to adjust the width in some cases.
 
 (function($) {
+    var slice = Array.prototype.slice; // save ref to original slice()
+    var splice = Array.prototype.splice; // save ref to original slice()
+
   var defaults = {
       topSpacing: 0,
       bottomSpacing: 0,
@@ -117,19 +120,21 @@
       update: scroller,
       unstick: function(options) {
         return this.each(function() {
-          var unstickyElement = $(this);
+          var that = this;
+          var unstickyElement = $(that);
 
           var removeIdx = -1;
-          for (var i = 0; i < sticked.length; i++)
+          var i = sticked.length;
+          while ( i-- > 0 ) 
           {
-            if (sticked[i].stickyElement.get(0) == unstickyElement.get(0))
+            if (sticked[i].stickyElement.get(0) == that)
             {
+                splice.call(sticked,i,1);
                 removeIdx = i;
             }
           }
           if(removeIdx != -1)
           {
-            sticked.splice(removeIdx,1);
             unstickyElement.unwrap();
             unstickyElement.removeAttr('style');
           }
@@ -148,7 +153,7 @@
 
   $.fn.sticky = function(method) {
     if (methods[method]) {
-      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+      return methods[method].apply(this, slice.call(arguments, 1));
     } else if (typeof method === 'object' || !method ) {
       return methods.init.apply( this, arguments );
     } else {
@@ -158,7 +163,7 @@
 
   $.fn.unstick = function(method) {
     if (methods[method]) {
-      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+      return methods[method].apply(this, slice.call(arguments, 1));
     } else if (typeof method === 'object' || !method ) {
       return methods.unstick.apply( this, arguments );
     } else {
